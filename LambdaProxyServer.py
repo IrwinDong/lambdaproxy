a16ba6010e0f192a000b110c3344faa4dd500a33
IrwinDong <xu.irwin.dong@gmail.com>
2020-02-23 21:25:47 -0800

    master

--- LambdaProxyServer.py
+++ LambdaProxyServer.py
@@ -19,7 +19,7 @@ class LambdaRedirectHandler(BaseHTTPRequestHandler):
         if(timeout):
             self.send_error(504, "proxy server timeout")
         else:
-            self.send_response(301)
+            self.send_response(302)
             self.send_header('Location', url)
             self.end_headers()
 
